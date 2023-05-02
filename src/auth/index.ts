9b82875d6c18b5b5596708c4b2cda27c2892c276
frederikprijck <frederik.prijck@auth0.com>
2023-05-02 13:50:42 +0200

    draft

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -3,6 +3,13 @@ import { Database } from './Database';
 import { OAuth } from './OAuth';
 import { Passwordless } from './Passwordless';
 
+export { default as Database } from './Database';
+export * from './Database';
+export { default as OAuth } from './OAuth';
+export * from './OAuth';
+export { default as Passwordless } from './Passwordless';
+export * from './Passwordless';
+
 export class AuthenticationClient {
   database: Database;
   oauth: OAuth;
---
+++ typedoc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  name: 'auth0',
+  out: './new-docs/',
+  exclude: [''],
+  excludeExternals: false,
+  excludePrivate: true,
+  hideGenerator: true,
+  readme: './README.md',
+  visibilityFilters: {
+    protected: false,
+    inherited: true,
+    external: true,
+  },
+};
